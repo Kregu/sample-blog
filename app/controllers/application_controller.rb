ddfb692f6c1e0486c8da2b204480c901a2ef41b7
Sergey Gorokhov <sergey.login@gmail.com>
2019-10-26 12:08:52 +0300

    Show user name in header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+    user_params.permit(:username, :email, :password, :password_confirmation)
+  end
   end
 end
