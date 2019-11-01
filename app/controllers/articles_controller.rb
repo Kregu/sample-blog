6091eaa2a2f3fe996233c5a689ab5e4b27118a93
Sergey Gorokhov <sergey.login@gmail.com>
2019-11-01 21:52:55 +0300

    Add method set_article to articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   
   def index
     @articles = Article.all
@@ -10,7 +11,6 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
     @users = User.all
   end
 
@@ -23,26 +23,22 @@ class ArticlesController < ApplicationController
     if @article.save
       redirect_to @article
     else
-      render action: 'new'
+      render :new
     end
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-
     if @article.update(article_params)
       redirect_to @article
     else
-      render action: 'edit'
+      render :edit
     end
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
@@ -51,7 +47,10 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :text)
+  end
 
+  def set_article
+     @article = Article.find(params[:id])
   end
 
 end
