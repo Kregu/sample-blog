393d88422ae0518f1180aa4c8f10fb625a7606fb
Sergey Gorokhov <sergey.login@gmail.com>
2019-11-04 00:14:54 +0300

    Add destroy action to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,16 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, :only => [:new, :create]
+  before_action :set_article, only: [:create, :destroy]
 
   def create
-    @article = Article.find(params[:article_id])
-    comment = @article.comments.new(author: current_user.username, body: comment_params[:body])
-    comment.save
-    
+    comment = @article.comments.create(author: current_user.username, body: comment_params[:body])
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+
     redirect_to article_path(@article)
 
   end
@@ -16,4 +21,7 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:body)
   end
 
+  def set_article
+    @article = Article.find(params[:article_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   
   resource :contacts, only: [:new, :create], path_names: { :new => ''}
   resources :articles do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
