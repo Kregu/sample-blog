4ad47142dc64e0d0d50d5a033fcd378382f7c76f
Sergey Gorokhov <sergey.login@gmail.com>
2019-11-09 01:04:08 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,7 +3,9 @@ require "rails_helper"
 describe Article do
 	describe "validations" do
 		it { should validate_presence_of :title }
-		it { should validate_presence_of :text }	
+		it { should validate_presence_of :text }
+		it { should validate_length_of(:title).is_at_most(140)}	
+		it { should validate_length_of(:text).is_at_least(5).is_at_most(4000)}	
 	end
 
 	describe "associacions" do
