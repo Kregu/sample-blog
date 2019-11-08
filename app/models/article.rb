19e40124b2d54231e1a2b377c3124b88c2c276a8
Sergey Gorokhov <sergey.login@gmail.com>
2019-11-09 01:03:52 +0300

    Update validations article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,8 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :user
   
-  validates :title, presence: true
-  validates :text, presence: true, length: {minimum: 5}
+  validates :title, presence: true, length: {maximum: 140}
+  validates :text, presence: true, length: {in: 5..4000}
 
   def subject
   	title
