c3e3ba0955d10f69fc184b678954c3eb8e658140
Sergey Gorokhov <sergey.login@gmail.com>
2019-11-09 22:17:36 +0300

    Update comment spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,12 @@
 require "rails_helper"
 
 describe Comment do
-	it { should belong_to :article }
+	describe "associations" do
+		it { should belong_to :article }
+	end
+
+	describe "validation" do
+		it {should validate_length_of(:body).is_at_least(5).is_at_most(4000)} 
+	end
 		
 end
