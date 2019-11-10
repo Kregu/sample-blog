820a1acb1675498f603dc45eb1f61b101cbb2f88
Sergey Gorokhov <sergey.login@gmail.com>
2019-11-10 13:03:07 +0300

    Update tests (allow a quest to create contact)

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -1,9 +1,18 @@
 require "rails_helper"
-require "capybara/rspec"
 
 feature "Contact Creation" do
-	scenario "allows acees to contacts page" do
-		visit new_contacts_path
-		expect(page).to have_content 'Contact us'
-	end	
+  scenario "allows accees to contacts page" do
+    visit new_contacts_path
+    expect(page).to have_content I18n.t 'contacts.contact_us'
+  end
+
+  scenario "allows a guest to create contact" do
+    visit new_contacts_path
+
+    fill_in 'contact_email', with: 'user@example.com'
+    fill_in 'contact_message', with: 'something'
+
+    click_button 'Send message'
+    expect(page).to have_content 'Thanks!'
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
@@ -8,6 +9,8 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'factory_bot'
+require 'capybara/rspec'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
