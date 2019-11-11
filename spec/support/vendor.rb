dcc1d5fb51bd71c2403f4297bb731f9b0b3a5837
Sergey Gorokhov <sergey.login@gmail.com>
2019-11-11 09:02:10 +0300

    Configure RSpec to use database_cleaner

---
+++ spec/support/vendor.rb
@@ -0,0 +1,17 @@
+
+# database_cleaner RSpec Example
+
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+end
